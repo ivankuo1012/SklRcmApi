bd26d66c8f466395bfa8d47122fcd2fb9c2f5b67
ivankuo1012 <ivankuo1012@gmail.com>
2020-12-14 11:44:11 +0800

    upload File

---
+++ SklRcmApi/Controllers/UploadFileController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+
+namespace SklRcmApi.Controllers
+{
+    public class UploadFileController : ApiController
+    {
+        public HttpResponseMessage Upload()
+        {
+            HttpResponseMessage result;
+            var httpRequest = HttpContext.Current.Request;
+
+            Debug.WriteLine("upload start");
+            var resultAry = new Dictionary<string, dynamic>();
+
+            if (httpRequest.Files.Count > 0)
+            {
+                var docfiles = new List<string>();
+                var postedFile = httpRequest.Files[0];
+                Debug.WriteLine(httpRequest.Files[0].FileName);
+                var filePath = HttpContext.Current.Server.MapPath("~/FileUploads/" + postedFile.FileName);
+                postedFile.SaveAs(filePath);
+
+                //foreach (string file in httpRequest.Files)
+                //{
+                //    var postedFile = httpRequest.Files[file]
+                //    //Debug.Write(httpRequest.Files[file]);
+                //    var filePath = HttpContext.Current.Server.MapPath("~/FileUploads/" + postedFile.FileName);
+
+                //    postedFile.SaveAs(filePath);
+                //    docfiles.Add(filePath);
+                //}
+
+                //Dictionary<string, dynamic> importDB = readExcelToDb(filePath);
+                resultAry.Add("file_name", postedFile.FileName);
+                resultAry.Add("file_path", filePath);
+                FileInfo fi = new FileInfo(filePath);
+                resultAry.Add("file_size", fi.Length);
+
+                //resultAry.Add("import_result", importDB);
+
+
+                result = Request.CreateResponse(HttpStatusCode.Created, resultAry);
+
+
+            }
+            else
+            {
+
+                result = Request.CreateResponse(HttpStatusCode.BadRequest);
+
+            }
+            return result;
+        }
+    }
+}
